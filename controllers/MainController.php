c2d64dcfd66601f89fd98b7973eaad4a02d08ac2
Ashzuu <evandynak@icloud.com>
2024-10-22 22:09:40 +0200

    fix: Missing files

---
+++ controllers/MainController.php
@@ -0,0 +1,18 @@
+<?php
+namespace Controllers;
+
+use League\Plates\Engine;
+
+class MainController {
+    private Engine $templates;
+
+    public function __construct() {
+        // Initialisation du moteur de templates Plates
+        $this->templates = new Engine('Views');
+    }
+
+    public function index(): void {
+        // Générer la vue 'home' avec un nom de set TFT
+        echo $this->templates->render('home', ['tftSetName' => 'Remix Rumble']);
+    }
+}
