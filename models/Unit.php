9a7aa3651f763f55f792b038dc87f3e61ab24fb9
Ashzuu <evandynak@icloud.com>
2024-10-22 22:56:55 +0200

    implement: Unit class & unitDAO class

--- models/Unit.php
+++ models/Unit.php
@@ -1,2 +1,51 @@
 <?php
 
+class Unit {
+    private string $id;
+    private string $name;
+    private int $cost;
+    private string $origin;
+    private string $url_img;
+
+    public function get_id():string {
+        return $this->id;
+    }
+
+    public function get_name():string {
+        return $this->name;
+    }
+
+    public function get_cost():int {
+        return $this->cost;
+    }
+
+    public function get_origin():string {
+        return $this->origin;
+    }
+
+    public function get_url_img():string {
+        return $this->url_img;
+    }
+
+
+    public function set_id(string $id):void {
+        $this->id = $id;
+    }
+
+    public function set_name(string $name):void {
+        $this->name = $name;
+    }
+
+    public function set_cost(int $cost):void {
+        $this->cost = $cost;
+    }
+
+    public function set_origin(string $origin):void {
+        $this->origin = $origin;
+    }
+
+    public function set_url_img(string $url_img):void {
+        $this->url_img = $url_img;
+    }
+
+}
--- models/UnitDAO.php
+++ models/UnitDAO.php
@@ -1,2 +1,33 @@
 <?php
+require_once 'BasePDODAO.php';
 
+class UnitDAO extends BasePDODAO {
+    public function getAll(){
+        $request = "SELECT * FROM unit";
+        $result = $this->execRequest($request);
+        $units = [];
+        foreach ($result as $row) {
+            $unit = new Unit();
+            $unit->set_id($row['id']);
+            $unit->set_name($row['name']);
+            $unit->set_cost($row['cost']);
+            $unit->set_origin($row['origin']);
+            $unit->set_url_img($row['url_img']);
+            $units[] = $unit;
+        }
+        return $units;
+    }
+
+    public function getById(string $id){
+        $request = "SELECT * FROM unit WHERE id = :id";
+        $result = $this->execRequest($request, [':id' => $id]);
+        $row = $result->fetch();
+        $unit = new Unit();
+        $unit->set_id($row['id']);
+        $unit->set_name($row['name']);
+        $unit->set_cost($row['cost']);
+        $unit->set_origin($row['origin']);
+        $unit->set_url_img($row['url_img']);
+        return $unit;
+    }
+}
